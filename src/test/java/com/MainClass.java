9b0d2840979883b31ae7798b27d62cbc89b1b323
Toshiba <mehmetsezginm@gmail.com>
2021-06-26 21:56:05 +0100

    2 nd iteration started.tc100

--- src/test/java/com/MainClass.java
+++ src/test/java/com/MainClass.java
@@ -4,6 +4,9 @@ public class MainClass {
     public static void main(String[] args) {
         System.out.println("This is the first team lead code");
         // Now updating on github.
+        //Check on IntelliJ, first loop completed
+        // 2 nd iteration started.
+
 
     }
 }
